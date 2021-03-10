9515cc3d6b2c5a057ee134dd4bc0a7d4ba043135
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-10 20:16:04 +0000

    Add user input

--- play.js
+++ play.js
@@ -2,19 +2,19 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
-const setupInput = function(data, handleUserInput) {
+const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', handleUserInput = (key) =>{
-    if (key === '\u0003') {
-      process.exit();
-    };
-  });
+  stdin.on('data', handleUserInput);
   return stdin;
 }
-
+const handleUserInput = (key) =>{
+  if (key === '\u0003') {
+    process.exit();
+  };
+}
 
 setupInput();
